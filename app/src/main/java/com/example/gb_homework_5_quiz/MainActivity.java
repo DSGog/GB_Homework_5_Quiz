149acfb18a57633cea4ae467db37dcaf37cfc1a3
DSGog <dmitriy.amsec@gmail.com>
2024-09-03 13:58:21 +0400

    Quiz 1

---
+++ app/src/main/java/com/example/gb_homework_5_quiz/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.gb_homework_5_quiz;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+//        MediaPlayer music = MediaPlayer.create(MainActivity.this, R.raw.air_raid);
+        MediaPlayer applause = MediaPlayer.create(MainActivity.this, R.raw.applause_y);
+//        music.start();
+        applause.start();
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
